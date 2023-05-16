9c3431ebd981c314bb8392536ebefbaf7a7281ce
MrPota2 <adrian.milvang@gmail.com>
2023-05-16 16:37:23 +0200

    Admin

--- scrape.py
+++ scrape.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup
 import re
 import pickle
 import datetime
-import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
