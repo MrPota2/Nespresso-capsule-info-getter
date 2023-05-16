ef1989b5522a6cfb5125ae70d018c8e06b1aaee9
MrPota2 <adrian.milvang@gmail.com>
2023-05-16 19:55:18 +0200

    Broken into defs with comments (not done)

--- scrape.py
+++ scrape.py
@@ -9,33 +9,37 @@ import time
 import mysql.connector
 import undetected_chromedriver as uc
 
-db = mysql.connector.connect(host='localhost', port=3306,
-                            user='py', passwd='pswd', db='Nespresso')
 
-url_file = open("urls.txt", "r")
-urls=[]
 
-line=url_file.readline()
-while line != "":
-    line=line.rstrip('\n')
-    urls+=[line]
+def get_urls():
+    """ 
+    open urls.txt and return a list of urls 
+    
+    Returns: list of urls 
+    """
+    url_file = open("urls.txt", "r")
+    urls = []
     line=url_file.readline()
-url_file.close()
+    while line != "":
+        line=line.rstrip('\n')
+        urls+=[line]
+        line=url_file.readline()
+    url_file.close()
+    return urls
 
-errlog = False
-count=0
-for url in urls:
+
+
+def open_url(url):
+    """
+    Open a url and returns the soup
+
+    url: url to open
+
+    Returns: soup
+    """
     driver = uc.Chrome(headless=True, version_main=113)
     error = []
-    print(len(urls)-count, "urls left")
     print('URL:',url)
-    # initiating the webdriver. Parameter includes the path of the webdriver.
-    
-    #options.add_argument("disable-infobars")
-    #options.add_argument("--disable-extensions")
-    #options.add_argument(f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
-    #options.add_argument('user-data-dir=C:/Users/adria/AppData/Local/Google/Chrome/User Data/Profile 1')
-    #driver = webdriver.Chrome(driver_exe, options=options)
 
     driver.get(url)
 
@@ -44,159 +48,165 @@ for url in urls:
     time.sleep(3)
 
     html = driver.page_source
-    
-
     soup = BeautifulSoup(html, "html.parser")
     print("Loaded:", soup.title)
-
-    try:
-        
-        try:
-            tech = soup.find("div", class_="cb-content").find("p", class_="cb-technology").get_text()
-            print(tech)
-        except AttributeError as e:
-            tech = "Unknown"
-            error += ("tech", e)
-        try:
-            rang = soup.find("div", class_="cb-content").find("p", class_="cb-range").get_text()
-            print(rang)
-        except AttributeError as e:
-            rang = "Unknown"
-            error += ("rang", e)
-        try:
-            title = re.sub(r'[\n]', '', soup.find("div", class_="cb-content").find("h1", class_="cb-heading").get_text())
-            print(title)
-        except AttributeError as e:
-            title = "Unknown"
-            error += ("title", e)
-        try:
-            text = soup.find("div", class_="cb-content").find("p", class_="cb-text").get_text()
-            print(text)
-        except AttributeError as e:
-            text = "Unknown"
-            error += ("text", e)
-        try:
-            intensity = re.sub(r'[\nIntensity ]', '', soup.find("div", class_="cb-content").find(
-            "nb-intensity").get_text())
-            print(intensity)
-        except AttributeError as e:
-            intensity = None
-            error += ("intensity", e)
-
-        try:
-            
-
-            # Test for size
-            fluid = soup.find("div", class_="nb-list__item-label").get_text()
-            if 'oz' in rang:
-                size = float(re.sub('[^0-9.]', '', rang))
-                size = size * 28.4130
-                rang = None
-            else:
-                size = None
-            print(size)
-
-        except AttributeError as e:
-            desc = "Unknown"
-            error+=("desc", e)
-            size = None
+    return soup, driver
 
 
-        
-        with open('land.pkl', 'rb') as f:
-            land = pickle.load(f)
 
-        desc = soup.find("div", class_="description").get_text()
-        #Test for country
-        country = ""
-        for i in land:
-            if land[i] in desc:
-                country = land[i]
-                break
-            else:
-                country = "Unknown"
+def find_data(query, driver):
+    """
+    Inform user of search query and return the result
 
+    query: soup search query
+    driver: selenium driver
 
-                
+    Returns: soup result
+    """
 
-        key = ('technology', 'range', 'title', 'text', 'intensity', 'size', 'country')
-        data = (tech, rang, title, text, intensity, size, country)
-
-        dic={}
-        for i in range(len(key)):
-            if data[i] != "":
-                dic[key[i]] = data[i]
-            else:
-                dic[key[i]] = "Unknown"
-
-        
-
-        try:
-            #Store in database    
-            cursor = db.cursor()
-            cursor.execute("SELECT * FROM Kapsel WHERE Navn = %s AND Teknologi = %s;", (dic['title'], dic['technology']))
-            result = cursor.fetchall()
-            cursor.close()
-            
-
-            if len(result) == 0:
-                insert_cursor = db.cursor()
-                insert_cursor.execute("INSERT INTO Kapsel (Teknologi, Rekke, Navn, Smak, Intensitet, Størrelse, Opphav) VALUES (%s, %s, %s, %s, %s, %s, %s);", (dic['technology'], dic['range'], dic['title'], dic['text'], dic['intensity'], dic['size'], dic['country']))
-                db.commit()
-                insert_cursor.close()
-                print("Inserted")
-            
-            else: 
-                if result[0][1] != dic['range'] or result[0][2] != dic['text'] or result[0][3] != dic['intensity'] or result[0][4] != dic['size'] or result[0][5] != dic['country']:
-                    update_cursor = db.cursor()
-                    update_cursor.execute("UPDATE Kapsel SET Rekke = %s, Smak = %s, Intensitet = %s, Størrelse = %s, Opphav = %s WHERE Teknologi = %s AND Navn = %s;", (dic['range'], dic['text'], dic['intensity'], dic['size'], dic['country'], dic['technology'], dic['title']))
-                    db.commit()
-                    update_cursor.close()
-                    print("Updated")
-                else:
-                    print("Already in database")
-
-            open('done.txt', 'a').write(url + '\n')
-            url_file_=open('urls.txt', 'w')
-            for i in urls:
-                if i != url:
-                    url_file_.write(i + '\n')
-            url_file_.close()
-                
-            
-        except mysql.connector.Error as e:
-            error += ("database", e)
-            print("Error: " + dic['title'] + "\n" + str(e))
-
-        if len(error) > 0:
-            if errlog == False:
-                timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-                err_file = open('./logs/error_' + timestamp + '.txt', 'w')
-                err_file.write('URL: ' + url + '\n')
-                for i in error:
-                    err_file.write('ERROR: ' + str(i) + '\n\n')
-                err_file.close()
-                errlog = True
-            else:
-                err_file = open('./logs/error_' + timestamp + '.txt', 'a')
-                err_file.write(url + '\n')
-                for i in error:
-                    err_file.write(str(i) + '\n')
-                err_file.close()
-
-
-    except AttributeError as e:
-        open('error.txt', 'a').write(url + '\n')
-        print("Error: " + url)
-
+    search = query
+    print("Searching for:", str(search))
+    print('Result:', search)
     driver.close()
-    count+=1
     print("\n\n")
-        
-if driver is not None:
-    driver.quit()
-else:
-    print("Driver not initialized")
+    return search
 
-db.close()
+def size_check(fluid):
+    """
+    Check if size is in the search result and convert to ml
+
+    fluid: soup search result
+
+    Returns: size in ml
+    """
+
+    # Test for size
+    if 'oz' in fluid:
+        ML = 28.4130
+        size_oz = float(re.sub('[^0-9.]', '', fluid))
+        size_ml = size_oz * ML
+        fluid = None
+    else:
+        size_oz = None
+        size_ml = None
+    print(size_ml)
+    return size_ml
+
+
+def country_check(desc):
+    """
+    Check if the origin country is in the search result
+
+    desc: soup search result (description)
+
+    Returns: origin country
+    """
+    with open('land.pkl', 'rb') as f:
+        land = pickle.load(f)
+    #Test for country
+    country = ""
+    for i in land:
+        if land[i] in desc:
+            country = land[i]
+        else:
+            country = "Unknown"
+    print(country)
+    return country
+
+def assemble_data(data):
+    """
+    Assemble data about the coffe into a dictionary
+
+    data: complete list of coffee attributes
+
+    Returns: dictionary containing all coffee data
+
+    not sure if this is useful
+    it definitely is... (I think). Maybe not. I don't know. I'm not sure. I'm confused.
+    it stays for now
+    """
+    
+    key = ('technology', 'range', 'title', 'text', 'intensity', 'size', 'country')
+    dic={}
+    for i in range(len(key)):
+        if data[i] != "":
+            dic[key[i]] = data[i]
+        else:
+            dic[key[i]] = "Unknown"
+    return dic
+
+def exist_check(dic, db):
+    """
+    Check if the coffee is already in the database
+
+    dic: dictionary containing all coffee data
+    db: database connection
+
+    Returns: True if coffee is in database, False if not
+    """
+    #Store in database    
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM Kapsel WHERE Navn = %s AND Teknologi = %s;", (dic['title'], dic['technology']))
+    existing_coffee = cursor.fetchall()
+    cursor.close()
+    if existing_coffee == []:
+        return False
+    else:
+        return True, existing_coffee
+
+def insert(dic, db):
+    """
+    Insert coffee into database
+    
+    dic: dictionary containing all coffee data
+    db: database connection
+    """
+    insert_cursor = db.cursor()
+    insert_cursor.execute("INSERT INTO Kapsel (Teknologi, Rekke, Navn, Smak, Intensitet, Størrelse, Opphav) VALUES (%s, %s, %s, %s, %s, %s, %s);", (dic['technology'], dic['range'], dic['title'], dic['text'], dic['intensity'], dic['size'], dic['country']))
+    db.commit()
+    insert_cursor.close()
+    print("Inserted")
+        
+
+def update(dic, existing_coffee, db):
+    """
+    Update coffee in database
+    
+    dic: dictionary containing all coffee data
+    
+    existing_coffee: result from exist_check
+    """
+    if existing_coffee[0][1] != dic['range'] or existing_coffee[0][2] != dic['text'] or existing_coffee[0][3] != dic['intensity'] or existing_coffee[0][4] != dic['size'] or existing_coffee[0][5] != dic['country']:
+        update_cursor = db.cursor()
+        update_cursor.execute("UPDATE Kapsel SET Rekke = %s, Smak = %s, Intensitet = %s, Størrelse = %s, Opphav = %s WHERE Teknologi = %s AND Navn = %s;", (dic['range'], dic['text'], dic['intensity'], dic['size'], dic['country'], dic['technology'], dic['title']))
+        db.commit()
+        update_cursor.close()
+        print("Updated")
+    else:
+        print("Already in database")
+
+
+def move(url):
+    """
+    Move url from urls.txt to done.txt
+    
+    url: url to move
+    """
+    urls = get_urls()
+    open('done.txt', 'a').write(url + '\n')
+    url_file_=open('urls.txt', 'w')
+    for i in urls:
+        if i != url:
+            url_file_.write(i + '\n')
+    url_file_.close()
+
+
+def main():
+    """
+    Main function (duh)
+    """
+    db = mysql.connector.connect(host='localhost', port=3306,
+                            user='py', passwd='pswd', db='Nespresso')
+
+    db.close()
 
